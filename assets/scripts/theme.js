d4af5a8f639f268b2633fdfec8f2f3c78595c67b
GangstaCat <rik.audenaert@outlook.com>
2022-05-11 15:49:56 +0200

    reordered

---
+++ assets/scripts/theme.js
@@ -0,0 +1,26 @@
+if (!localStorage.getItem("theme")) {
+  localStorage.setItem("theme", "light");
+}
+
+setInterval(() => {
+  let theme = document.getElementById("themeSelect").value;
+  const body = document.body;
+  if (theme === "light") {
+    body.className = "light";
+    localStorage.setItem("theme", "light");
+  } else if (theme === "dark") {
+    body.className = "dark";
+    localStorage.setItem("theme", "dark")
+  } else if (theme === "dynamic") {
+    localStorage.setItem("theme", "dynamic");
+    const date = new Date();
+    if (date.getHours() >= 19 || date.getHours() < 7) {
+      body.className = "dark";
+    } else if (date.getHours() < 19 && date.getHours() >= 7) {
+      body.className = "light";
+    } else {
+      alert("What the fuck are you doing");
+    }
+    theme = localStorage.getItem("theme");
+  }
+}, 10);
