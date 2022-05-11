c706a1cf07c5d8970b2af5fc1577618318ed47f1
Flux <flustixshinu@gmail.com>
2022-05-11 17:13:19 +0200

    new theme selection

--- assets/scripts/theme.js
+++ assets/scripts/theme.js
@@ -1,16 +1,15 @@
+themes = ['light', 'dark', 'dynamic'];
+
 function loadTheme() {
   if (!localStorage.getItem("theme")) {
     localStorage.setItem("theme", "light");
   }
   const savedTheme = localStorage.getItem("theme");
-  document.getElementById("themeSelect").value = savedTheme;
-
-  setInterval(() => {
-    setTheme(document.getElementById("themeSelect").value);
-  }, 10);
+  setTheme(themes.indexOf(savedTheme));
 }
 
-function setTheme(style) {
+function setTheme(themeid) {
+  var style = themes[themeid];
   localStorage.setItem("theme", style);
 
   if (style == "dynamic") {
@@ -25,4 +24,5 @@ function setTheme(style) {
   }
 
   document.body.className = style;
+  document.getElementById("selectedThemeIndicator").style.left = 100 * themeid + (7.78333 * themeid) + "px";
 }
