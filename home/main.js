c1384b7c079b56e45867d175b56be5813b1a9c26
GangstaCat <rik.audenaert@outlook.com>
2023-05-31 20:34:58 +0200

    website V5.1

--- home/main.js
+++ home/main.js
@@ -1,4 +1,5 @@
 function copy() {
+  // copy discord tag to clipboard and change the text
   navigator.clipboard.writeText("GangstaCat#0458");
   var tooltip = document.getElementById("myTooltip");
   tooltip.innerHTML = "Copied to clipboard";
@@ -6,6 +7,7 @@ function copy() {
 
 
 function outFunc() {
+  // change text back when the mouse moves away
   var tooltip = document.getElementById("myTooltip");
   tooltip.innerHTML = "Copy to clipboard";
 }
