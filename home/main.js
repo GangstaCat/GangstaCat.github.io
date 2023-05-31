c26213ede510dfd764bac48b5ce74d4f56babebe
GangstaCat <rik.audenaert@outlook.com>
2023-05-31 18:49:55 +0200

    home page V1

---
+++ home/main.js
@@ -0,0 +1,11 @@
+function copy() {
+  navigator.clipboard.writeText("GangstaCat#0458");
+  var tooltip = document.getElementById("myTooltip");
+  tooltip.innerHTML = "Copied to clipboard";
+}
+
+
+function outFunc() {
+  var tooltip = document.getElementById("myTooltip");
+  tooltip.innerHTML = "Copy to clipboard";
+}
